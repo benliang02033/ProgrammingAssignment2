d66b4df29e0f963b306af59d6cd932e326d42725
Jinbin Liang <jinbinliang@Jinbins-MacBook-Air.local>
2018-11-08 15:47:24 -0800

    Revert "cachematrix.R"
    This reverts commit 5f3f101d5363fe68ba327a8c8e221881ddcfc979.

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-m <- NULL
-  set<-function(y){
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) m <<- solve(x)
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+
 }
 
 
@@ -22,13 +12,4 @@ m <- NULL
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-  if(!is.null(m)){
-    print("Getting the cache inverse.")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
 }
